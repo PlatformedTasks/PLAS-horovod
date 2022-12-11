fa01851a4eabaf2a35ae4b6488b476d375c982d7
ludovico.funari <ludovico.funari@gmail.com>
2022-12-11 17:08:50 +0000

    Clean code

--- horovod-executor2.py
+++ horovod-executor2.py
@@ -8,14 +8,19 @@ try:
     os.mkdir("/root/.ssh", 0o600)
     shutil.copyfile("/tmp/generated/executor.init", "/root/.ssh/id_rsa")
     os.chmod("/root/.ssh/id_rsa", 0o400)
-    
+
+    python_script = sys.argv[2]
+    trainingset = sys.argv[3]
+    training_metrics = sys.argv[4]
+    net_weights = sys.argv[5]
+
     horovod_command = sys.argv[1].split(" ")
     horovod_command.insert(1, "--hostfile")
     horovod_command.insert(2, "/tmp/generated/executor.config")
-    horovod_command.extend(["python",argv[2],argv[3],argv[4],argv[5])
+    horovod_command.extend(["python", python_script, trainingset, training_metrics, net_weights])
 
     print("horovod_command:", horovod_command)
-    
+
     process = subprocess.run(horovod_command)
 except Exception as err:
     print(err)
